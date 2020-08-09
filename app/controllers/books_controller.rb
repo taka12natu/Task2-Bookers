2964049164d7a77b40e0720a1f27a29985c1d88a
taka12natu <hvhpdrmcmtr12@gmail.com>
2020-08-09 12:25:48 +0900

    first update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,37 @@
 class BooksController < ApplicationController
-  def new
+  
+   def index
+  	@book = Book.new
+  end
+
+  def create
+  	@book = Book.new(book_params)
+  	if @book.save
+  	redirect_to @book, notice: 'success!'
+  	else
+  		render :index
+  	end
   end
 
   def edit
+  	@book = Book.find(params[:id])
   end
 
   def show
+  	@book = Book.find(params[:id])
   end
 
-  def index
+  def update
+  	@book = Book.find(params[:id])
+  	@book.update(book_params)
+  	redirect_to book_path(@book)
   end
 
   def top
   end
+
+  private
+  	def book_params
+  		params.require(:book).permit(:title, :body)
+    end
 end
